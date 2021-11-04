30cf53320c468e5d48b49dab9e55c5c4925faab3
Andrey Kovalenko <adrkov02@gmail.com>
2021-11-04 14:39:49 +0200

    write migration but did not generate

---
+++ database/migrations/2021_11_03_185410_create_ingridients_coctails_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIngridientsCoctailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ingridients_coctails', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('ingridient_id')
+                ->unsigned()
+                ->index();
+            $table->bigInteger('coctail_id')
+                ->unsigned()
+                ->index();
+            $table->string('count_of_ingridient',50);
+
+            $table->foreign('ingridient_id')
+                ->references('id')
+                ->on('ingridients');
+            $table->foreign('coctail_id')
+                ->references('id')
+                ->on('coctails');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('ingridients_coctails');
+    }
+}
---
+++ database/migrations/2021_11_03_185426_create_coctails_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoctailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coctails', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',255);
+            $table->text('description',500);
+            $table->text('recipe',500);
+            $table->string('photo',255);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('coctails');
+    }
+}
