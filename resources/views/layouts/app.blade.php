7d370d594010f6893da4e87774a661f089c0bbe3
Andrey Kovalenko <adrkov02@gmail.com>
2021-11-05 21:00:44 +0200

    create page bars

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
 
     <style>
@@ -22,6 +23,14 @@
 
         .fa-btn {
             margin-right: 6px;
+        }
+         table,tr,td{
+             margin: 35px;
+         }
+        .imgOfBar{
+            border-radius: 70px;
+            height:200px;
+            margin: 7px;
         }
     </style>
 </head>
@@ -40,14 +49,17 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Laravel
+                    Plunk
                 </a>
             </div>
 
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Home</a></li>
+                    <li><a href="{{ url('/home') }}">Коктейли</a></li>
+                    <li><a href="{{ url('/main/bars') }}">Игридиеты</a></li>
+                    <li><a href="{{ url('/home') }}">Бары Днепра</a></li>
+                    <li><a href="{{ url('/home') }}">Корзина</a></li>
                 </ul>
 
                 <!-- Right Side Of Navbar -->
@@ -78,5 +90,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+{{--@include('footer.blade.php')--}}
 </body>
 </html>
---
+++ resources/views/main/bars.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+
+                <div class="panel-body">
+                    <table>
+                        <tr>
+                            <td rowspan="2"><img src="https://media-cdn.tripadvisor.com/media/photo-s/0d/69/ec/06/caption.jpg" class="imgOfBar" ></td>
+                            <td><h2>Bartolomeo</h2></td>
+                        </tr>
+                        <tr>
+                            <td><h4>Адрес: вулиця Набережна Перемоги, 9Б, Дніпро, Дніпропетровська область, 49000</h4></td>
+                        </tr>
+                        <tr>
+                            <td rowspan="2"><img src="https://10619-2.s.cdn12.com/rests/original/313_509842704.jpg" class="imgOfBar"></td>
+                            <td><h2>SOHO Restaurant & Bar</h2></td>
+                        </tr>
+                        <tr>
+                            <td><h4>
+                                    Адрес: проспект Дмитра Яворницького, 47А, Дніпро, Дніпропетровська область, 49000
+                                    </h4></td>
+                        </tr>
+                        <tr>
+                            <td rowspan="2"><img src="https://i.pinimg.com/originals/fe/10/57/fe1057398238adaff5a5417c162db0df.jpg" class="imgOfBar" ></td>
+                            <td><h2>Old Pal</h2></td>
+                        </tr>
+                        <tr>
+                            <td><h4>Адрес: вулиця Січових Стрільців, 3, Дніпро́, Дніпропетровська область, 49000</h4></td>
+                        </tr>
+                        <tr>
+                            <td rowspan="2"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsNgWBELkOrMobFqv_CFpaudNhTASc9nnooSOMLBgO-D4MegKKR3Mf57Y8djjgKDvSSCk&usqp=CAU" class="imgOfBar" ></td>
+                            <td><h2>TWINS bar</h2></td>
+                        </tr>
+                        <tr>
+                            <td><h4>Адрес: проспект Дмитра Яворницького, 40, Дніпро, Дніпропетровська область, 49000</h4></td>
+                        </tr>
+                        <tr>
+                            <td rowspan="2"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSA8f3oJEa58Cav4uQGye4yJDnV3_LEnjNMIFdVwWwZ4fiUNSWcPBbMNDdtciEyM0a10RU&usqp=CAU" class="imgOfBar" ></td>
+                            <td><h2>BIG BEN</h2></td>
+                        </tr>
+                        <tr>
+                            <td><h4>Адрес: Троїцька площа, 3, Дніпро, Дніпропетровська область, 49000</h4></td>
+                        </tr>
+                        <tr>
+                            <td rowspan="2"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeZtf737SdK1sXkQtOvJ3-qG3mJ3z7kt_srn1zSwPWo2NX3OqPou35lIVEQnL1dndE4NU&usqp=CAU" class="imgOfBar" ></td>
+                            <td><h2>Black Bar</h2></td>
+                        </tr>
+                        <tr>
+                            <td><h4>Адрес: проспект Дмитра Яворницького, 77, Дніпро, Дніпропетровська область, 49000</h4></td>
+                        </tr>
+                    </table>
+                    <footer class="navbar navbar-default ">
+                            <div class="navbar-header">
+                                <h5>Laravel final project php 21-1 @</h5>
+                        </div>
+                    </footer>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
