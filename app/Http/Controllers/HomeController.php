8c7e5078c6599959fbd183edab13b443019e3170
Korchevskaja Julyja <julija.korchevskaja@privatbank.ua>
2021-11-07 22:15:55 +0200

    Edit HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('cocktails.createcocktail');
+//        return view('home');
     }
 }
