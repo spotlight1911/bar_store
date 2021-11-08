1414c400804677703608ecd23b5e73e783347dc7
MihailExplain <unkvuzutor@gmail.com>
2021-11-08 18:42:42 +0200

    make function index

--- app/Http/Controllers/IngredientsController.php
+++ app/Http/Controllers/IngredientsController.php
@@ -8,5 +8,13 @@ use App\Http\Requests;
 
 class IngredientsController extends Controller
 {
-    //
+    /**
+     * Display a ingredients of the resource.
+     * @param Ingredient $ingredients
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Ingredient $ingredients){
+//        return view('ingredients.index', ['ingredients' => $ingredients]);
+        return view('ingredients.index');
+    }
 }
