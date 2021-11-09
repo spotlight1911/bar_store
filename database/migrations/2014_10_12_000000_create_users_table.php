e0b4c52321efd98db625a15f962014ff2a1c9a2a
Korchevskiy <korhyn@gmail.com>
2021-11-09 14:06:07 +0200

    debag

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->boolean('is_admin')->default('0');
         });
     }
 
