77b9fda9ded7029487adea7bcc71719620e8ad2d
MihailExplain <unkvuzutor@gmail.com>
2021-11-09 15:37:05 +0200

    modification admin panel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,9 +60,11 @@
                     <li><a href="{{ url('/home') }}">Игридиеты</a></li>
                     <li><a href="{{ url('/main/bars') }}">Бары Днепра</a></li>
                     <li><a href="{{ url('/home') }}">Корзина</a></li>
+                    @if(Auth::user())
                         @if(Auth::user()->isAdmin())
                             <li><a href="{{ url('/admin/add') }}">Admin Panel</a></li>
                         @endif
+                    @endif
                 </ul>
 
                 <!-- Right Side Of Navbar -->
