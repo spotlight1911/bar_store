bcac7a32b8553d831e4df842f5125d926311d4a2
MihailExplain <unkvuzutor@gmail.com>
2021-11-12 15:26:00 +0200

    new CheckIsAdmin

--- app/Http/Middleware/CheckIsAdmin.php
+++ app/Http/Middleware/CheckIsAdmin.php
@@ -3,10 +3,9 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
 
-class СheckIsAdmin
+class CheckIsAdmin
 {
     /**
      * Handle an incoming request.
