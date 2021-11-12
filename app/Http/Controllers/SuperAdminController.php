b68ef2747a9685438a1e6b8f7e4db9a7bde2f64b
MihailExplain <unkvuzutor@gmail.com>
2021-11-12 17:03:43 +0200

    Make super admin KRUD

--- app/Http/Controllers/SuperAdminController.php
+++ app/Http/Controllers/SuperAdminController.php
@@ -13,11 +13,18 @@ class SuperAdminController extends Controller
         $users = User::all();
         return(view('admin.user', ['users'=>$users]));
     }
-    public function editIngredients(Ingridient $ingridient){
-        return view('admin.edit', ['ingredient' => $ingridient,]);
+    public function adminTakeRightsUser(User $user){
+        $user->is_admin = 0;
+        $user->save();
+        return $this->indexUsers();
     }
-    public function adminRightsUser(User $user){
+    public function adminGiveRightsUser(User $user){
         $user->is_admin = 1;
+        $user->save();
+        return $this->indexUsers();
+    }
+    public function deleteUser(User $user){
+        $user->delete();
         return $this->indexUsers();
     }
 }
--- resources/views/admin/user.blade.php
+++ resources/views/admin/user.blade.php
@@ -29,21 +29,37 @@
                         <div>{{$user->email}}</div>
                     </td>
                     <td class="table-text"></tdclass>
-                        <div>{{$user->is_admin}}</div>
+                        <div>@if($user->is_admin === 1)
+                                 Admin
+                            @elseif($user->is_admin === 2)
+                                 Super Admin
+                            @else
+                                 User
+                                 @endif
+{{--                            {{$user->is_admin}}--}}
+                        </div>
                     </td>
                     <td>
-                        <form method="post" action="{{route('user.edit', $user->id)}}">
+                        @if($user->is_admin !== 2)
+                        <form method="post" action="{{route('user.takeRights', $user->id)}}">
                             {{ csrf_field() }}
                             {{method_field('GET')}}
-                            <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                            <button class="btn btn-warning"><i class="fa fa-close"></i></button>
                         </form>
-{{--                        <form method="post" action="{{route('ingredient.destroy', $ingridient->id)}}">--}}
-{{--                            {{ csrf_field() }}--}}
-{{--                            {{method_field('DELETE')}}--}}
-{{--                            <button class="btn btn-danger"><i class="fa fa-trash"></i></button>--}}
-{{--                        </form>--}}
-
+                        <form method="post" action="{{route('user.giveRights', $user->id)}}">
+                            {{ csrf_field() }}
+                            {{method_field('GET')}}
+                            <button class="btn btn-success"><i class="fa fa-check"></i></button>
+                        </form>
                     </td>
+                    <td>
+                        <form method="post" action="{{route('user.destroy', $user->id)}}">
+                            {{ csrf_field() }}
+                            {{method_field('DELETE')}}
+                            <button class="btn btn-danger"><i class="fa fa-trash"></i></button>
+                        </form>
+                    </td>
+                    @endif
                 </tr>
             @endforeach
             </tbody>
